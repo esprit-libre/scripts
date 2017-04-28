f109c5d72f8ca3d4b47bd84c75bc39c7401d5785
esprit-libre <contact@esprit-libre-conseil.com>
2017-04-28 11:17:41 +0200

    Create links by date instead of daily.0...

--- rsnapshot-date-links.sh
+++ rsnapshot-date-links.sh
@@ -2,14 +2,14 @@
 
 ## Links by dates for readability of rsnapshot folders
 #
-# version 1.0.0 - 27/04/2017
+# version 1.0.1 - 28/04/2017
 #
 # This script create links for rsnapshot sub-directories replacing daily.0
 # by the date of creation of this folder.
 #
 # Usage:
-#  `./rsnapshot-date-link.sh -l LINKS_PATH -r RSNAPSHOT_PATH [-d DEEP] [-v]
-#  `./rsnapshot-date-link.sh --links LINKS_PATH --rsnap RSNAPSHOT_PATH [--deep DEEP] [--verbose]
+#  `./rsnapshot-date-link.sh -l LINKS_PATH -s SNAPSHOT_PATH [-d DEEP] [-r] [-v]
+#  `./rsnapshot-date-link.sh --links LINKS_PATH --snapshot SNAPSHOT_PATH [--deep DEEP] [--relative] [--verbose]
 #
 # Examples:
 # `./rsnapshot-date-link.sh -l /home/user/links -r /data/user/rsnapshot -v -d 3
@@ -51,8 +51,9 @@ function read_vars() {
     echo -e '\033[1m#-- read vars\033[0m'
 
     LINKS_PATH=""
-    RSNAPSHOT_PATH=""
+    SNAPSHOT_PATH=""
     DEEP=0
+    RELATIVE='false'
     LOG='false'
 
     while [[ $# -gt 0 ]]
@@ -71,13 +72,13 @@ function read_vars() {
             fi
             shift
             ;;
-        -r|--rsnap)
+        -s|--snapshot)
             if [ -d "$2" ]; then
                 # ${VAR//ab/yz} replace all sub-chains 'ab' from VAR by 'yz'
-                RSNAPSHOT_PATH="${2}/"
-                RSNAPSHOT_PATH="${RSNAPSHOT_PATH//'//'//}"
+                SNAPSHOT_PATH="${2}/"
+                SNAPSHOT_PATH="${SNAPSHOT_PATH//'//'//}"
             else
-                echo "[ERROR] Invalid RSNAPSHOT_PATH dir : "$2
+                echo "[ERROR] Invalid SNAPSHOT_PATH dir : "$2
                 exit 1
             fi
             shift
@@ -87,6 +88,9 @@ function read_vars() {
                 DEEP="$2"
             fi
             shift
+            ;;
+        -r|--relative)
+            RELATIVE='true'
             ;;
         -v|--verbose)
             LOG='true'
@@ -98,7 +102,7 @@ function read_vars() {
     shift
     done
 
-    test ${LOG} = 'true' && echo "[LOG] LINKS_PATH=\""${LINKS_PATH}"\" ; RSNAPSHOT_PATH=\""${RSNAPSHOT_PATH}"\" ; DEEP="${DEEP}
+    test ${LOG} = 'true' && echo "[LOG] LINKS_PATH=\""${LINKS_PATH}"\" ; SNAPSHOT_PATH=\""${SNAPSHOT_PATH}"\" ; DEEP="${DEEP}"\" ; RELATIVE="${RELATIVE}
 
     return 0
 }
@@ -116,33 +120,65 @@ function clean_links() {
 function create_links() {
     echo -e '\033[1m#-- create links\033[0m'
 
-	counting=1
+	test ${RELATIVE} = 'true' && cd "${LINKS_PATH}"
+
+	LINK_NUM=1
 	# sed 's,/$,,g' : remove ending /
 	# ls -Adt : list folders except . and .. and sort them by modify date
-	ls -Adt "${RSNAPSHOT_PATH}"/*/ | sed 's,/$,,g' | while read fold; do
-		# `printf %03d $((counting))` : converts 1 digit number to 3 digit number
-		linkname=$(date -r "${fold}" "+%Y.%m.%d_%Hh%Mmin")"_"`printf %03d $((counting))`
-		
+	ls -Adt "${SNAPSHOT_PATH}"/*/ | sed 's,/$,,g' | while read FOLD_TO_LINK; do
+		# `printf %03d $((LINK_NUM))` : converts 1 digit number to 3 digit number
+		LINKNAME=$(date -r "${FOLD_TO_LINK}" "+%Y.%m.%d_%Hh%Mmin")"_"`printf %03d $((LINK_NUM))`
+
 		DEPTH=${DEEP}
 		while [ ${DEPTH} -gt 0 ] # Explore inside dir at indicated level
 		do
 			shopt -s nullglob	# see: http://stackoverflow.com/questions/18884992/how-do-i-assign-ls-to-an-array-in-linux-bash
-			TEMP=("${fold}"/*/)
+			TEMP=("${FOLD_TO_LINK}"/*/)
 			shopt -u nullglob
-			#test ${LOG} = 'true' && echo "[LOG] ls explo["${DEPTH}"]: "${TEMP}" ("${#TEMP[@]}")"
 			if [ ${#TEMP[@]} -eq 1 ]; then
-				fold="${TEMP}"
+				FOLD_TO_LINK="${TEMP}"
 			fi
 			((DEPTH--))
 		done
 
-		fold="${fold//'//'/'/'}"				# clean // into /
-		LINKS_PATH="${LINKS_PATH//'//'/'/'}"	# clean // into /
-		ln -s "${fold}" "${LINKS_PATH}"/"${linkname}"
-		((counting++))
-		
-		test ${LOG} = 'true' && echo "[LOG] Folder: "${fold}
-		test ${LOG} = 'true' && echo "[LOG] |_Link: "${linkname}
+		FOLD_TO_LINK="${FOLD_TO_LINK//'//'/'/'}"	# clean // into /
+		LINKS_DIR="${LINKS_PATH//'//'/'/'}"			# clean // into /
+
+		# relative symlink
+		if [ ${RELATIVE} = 'true' ]; then
+			#test ${LOG} = 'true' && echo "[LOG] original LINKS_DIR="${LINKS_DIR}" ; SNAPSHOT_PATH="${FOLD_TO_LINK}
+			COMPARE='true'
+			NB_REL=0
+			LINKS_DIR="${LINKS_DIR#*/}"			# removes first /
+			FOLD_TO_LINK="${FOLD_TO_LINK#*/}"	# removes first /
+			while $COMPARE
+			do
+				# Compare first part of PATHS
+				if [ "${LINKS_DIR%%/*}" = "${FOLD_TO_LINK%%/*}" ]; then
+					LINKS_DIR="${LINKS_DIR#*/}"			# removes first bloc of PATH
+					FOLD_TO_LINK="${FOLD_TO_LINK#*/}"	# removes first bloc of PATH
+					((NB_REL++))
+				else
+					COMPARE='false'
+				fi
+			done
+			test ${NB_REL} -eq 0 && FOLD_TO_LINK="/${FOLD_TO_LINK}"	# if no part of PATH are similar, restore initial /
+			while [ ${NB_REL} -gt 0 ]
+			do
+				FOLD_TO_LINK="../${FOLD_TO_LINK}"
+				((NB_REL--))
+			done
+
+			ln -s "${FOLD_TO_LINK}" "${LINKNAME}"
+
+		# absolute symlink
+		else
+			ln -s "${FOLD_TO_LINK}" "${LINKS_DIR}"/"${LINKNAME}"
+		fi
+
+		((LINK_NUM++))
+
+		test ${LOG} = 'true' && echo "[LOG] link created: "${LINKNAME}" -> "${FOLD_TO_LINK}
 	done
 
 	return 0
