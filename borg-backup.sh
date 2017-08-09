8089a3daa5263b91ce326ad3d8b76580cf93fe01
esprit-libre <contact@esprit-libre-conseil.com>
2017-08-09 16:29:52 +0200

    Script that automates MariaDB export and BorgBackup to a remote server

--- borg-backup.sh
+++ borg-backup.sh
@@ -1,45 +1,154 @@
-#-----------
-# Template script from https://code.crapouillou.net/snippets/1
-#-----------
+#!/bin/bash
+set -e
 
-set -e	# info : http://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
-cleanup()
-{
-    echo "Something bad happened during backup, check ${LOG_PATH}"
-    exit $1
+readonly VERSION='1.0'
+readonly DATE='9 aug. 2017'
+readonly LOG_PATH=/var/log/borg-user.log
+readonly BORG=/usr/bin/borg
+
+CURRENT_DATE=$(date +%Y%m%d_%H%M%S)
+
+version() {
+	echo "$(basename "${0}") - version ${VERSION} - ${DATE}."
 }
-ts_log()
-{
-    echo `date '+%Y-%m-%d_%H:%M:%S'`" - "$1 >> ${LOG_PATH}
+
+usage() {
+	echo ''
+	echo '$(basename "${0}") -s[erver] <remote_server> -u[ser] <remote_user> -f[olders] <dir_to_backup> [ OPTIONS ]'
+	echo ''
+	echo 'OPTIONS = { -v[ersion] | -h[elp] | -d[ebug] }'
+	echo 'OPTIONS = { -n[obase] } : no databases backup, even if -m and -b are set'
+	echo 'OPTIONS = { -m[yuser] <mysql_user> } : use with -b option. If not set, root is used.'
+	echo 'OPTIONS = { -b[ase] <mysql_base> } : use with -m option. If not set, all databases are dumped.'
+	echo ''
+	echo 'This script intends to backup folders and MariaDB databases to a remote server'
+	echo 'using BorgBackup software.'
+	echo ''
 }
-trap '[ "$?" -eq 0 ] || cleanup' EXIT	# Trap on non-zero exit
 
-ts_log "Starting new backup ${BACKUP_DATE}..."
+log() {
+	if [ -n ${DEBUG} ]; then
+		echo -e ${@}
+	fi
+	echo -e ${@} >> ${LOG_PATH}
+}
 
-BACKUP_DATE=`date +%Y%m%d_%H%M%S`
-LOG_PATH=/var/log/borg-user.log
+init() {
+	readonly  ERR="[\033[1;31mERROR\033[0m]	"
+	readonly   OK="[\033[1;32mOK\033[0m]	"
+	readonly WARN="[\033[1;33mWARN\033[0m]	"
+	readonly INFO="[\033[36mINFO\033[0m]	"
+}
 
-BORG=/usr/bin/borg
-export BORG_RSH="ssh -i /root/.ssh/user_id_rsa"
-export BORG_PASSPHRASE="`cat /root/.borg/passphrase`"
-#BORG_REPOSITORY=user@server.com:/data/user/borg/servername
-BORG_REPOSITORY=sshname:/data/user/borg/servername
-BORG_ARCHIVE=${BORG_REPOSITORY}::${BACKUP_DATE}
+parameters() {
+	while [[ $# -gt 0 ]]; do
+		local key="$1"
+		case $key in
+			-b|-base)
+				readonly MYSQLBASE="${2}"
+				shift
+				;;
+			-d|-debug)
+				DEBUG='true'
+				log "${INFO}Debug logs activated"
+				;;
+			-f|-folders)
+				readonly FOLDERS="${2}"
+				shift
+				;;
+			-h|--help)
+				usage
+				exit 0
+				;;
+			-m|-myuser)
+				readonly MYSQLUSER="${2}"
+				shift
+				;;
+			-n|-nobase)
+				NOBASE='true'
+				;;
+			-s|-server)
+				readonly REMOTESERVER="${2}"
+				shift
+				;;
+			-u|-user)
+				readonly REMOTEUSER="${2}"
+				shift
+				;;
+			-v|--version)
+				version
+				exit 0
+				;;
+			*)
+				log "${WARN}Unkown option: $1"
+				;;
+		esac
+		shift
+	done
 
-ts_log 'Dumping MySQL db...'
-MYSQL_PASS=`cat /root/.borg/mysql_user`
-MYSQL_USER="mysqluser"
-MYSQL_DATABASE="database"
-MYSQL_TMP_DUMP_FILE=/temp/path/mysql_all_db.sql
+	if [ -z "${REMOTESERVER}" ]; then
+		log "${ERR}Missing remote server info."
+		usage
+		return 1
+	fi
+	if [ -z "${REMOTEUSER}" ]; then
+		log "${ERR}Missing remote user info."
+		usage
+		return 2
+	fi
+	if [ -z "${FOLDERS}" ]; then
+		log "${ERR}Missing folders to save info."
+		usage
+		return 2
+	fi
+	if [ -n "${MYSQLBASE}" -a -z "${MYSQLUSER}" ]; then
+		log "${ERR}MySQL user info missing. Should be BASE+USER or none (=dump all)."
+		usage
+		return 3
+	elif [ -n "${MYSQLUSER}" -a -z "${MYSQLBASE}" ]; then
+		log "${ERR}MySQL base info missing. Should be BASE+USER or none (=dump all)."
+		usage
+		return 4
+	elif [ -n "${MYSQLUSER}" -a -n "${MYSQLBASE}" ]; then
+		if [ -f "/root/.borg/mysql_${MYSQLUSER}" ]; then
+			MYSQLPASS=`cat /root/.borg/mysql_${MYSQLUSER}`
+		else
+			log "${ERR}MySQL password missing. Please fill \"~/.borg/mysql_${MYSQLUSER}\" file."
+			return 5
+		fi
+	fi
+}
 
-#mysqldump --all-databases --events -p$MYSQL_PASS > $MYSQL_TMP_DUMP_FILE
-mysqldump -u$MYSQL_USER --events -p$MYSQL_PASS $MYSQL_DATABASE > $MYSQL_TMP_DUMP_FILE
+init
+parameters "$@"
 
-ts_log "Pushing archive ${BORG_ARCHIVE}"
+export BORG_RSH="ssh -i /root/.ssh/${REMOTESERVER}"
+export BORG_PASSPHRASE="`cat /root/.borg/${REMOTESERVER}`"
+BORG_REPOSITORY="${REMOTESERVER}":/data/"${REMOTEUSER}"/borg/$(hostname)
+BORG_ARCHIVE=${BORG_REPOSITORY}::${CURRENT_DATE}
+
+log "${INFO}Dumping MariaDB databases..."
+if [ ! -d /tmp/"${REMOTESERVER}"_"${REMOTEUSER}" ]; then
+	mkdir /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"
+elif [ $( ls -1 /tmp/"${REMOTESERVER}"_"${REMOTEUSER}" | wc -l ) -gt 0 ]; then
+	rm -r /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"/*
+fi
+
+if [ -z "${NOBASE}" -a -n "${MYSQLBASE}" ]; then
+	mysqldump -u"${MYSQLUSER}" -p"${MYSQLPASS}" "${MYSQLBASE}" > /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"/"${MYSQLBASE}"_${CURRENT_DATE}.sql
+elif [ -z "${NOBASE}" ]; then
+	mysql -e 'SHOW DATABASES;' | grep -v Database | grep -v performance_schema | grep -v information_schema | grep -v mysql > /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"/base_list
+	{ while read u1; do
+		log "${INFO}MariaDB ${u1} database extraction..."
+		mysqldump "${u1}" > /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"/"${u1}"_${CURRENT_DATE}.sql
+	done ; } < /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"/base_list
+fi
+
+log "${INFO}Pushing archive ${BORG_ARCHIVE}"
 $BORG create \
      -v --stats --compression lzma,9 \
      $BORG_ARCHIVE \
-     /etc /var/www /home $MYSQL_TMP_DUMP_FILE \
+     "${FOLDERS}" /tmp/"${REMOTESERVER}"_"${REMOTEUSER}" \
      --exclude '/home/*/.ssh' \
      --exclude '/home/*/cache' \
      --exclude '/root/.borg' \
@@ -53,5 +162,5 @@ $BORG prune -v $BORG_REPOSITORY \
       --keep-monthly=6 \
       >> ${LOG_PATH} 2>&1
 
-ts_log 'Cleaning up...'
-rm $MYSQL_TMP_DUMP_FILE
+log "${INFO}Cleaning up..."
+rm -r /tmp/"${REMOTESERVER}"_"${REMOTEUSER}"
