47e2c3b5a9d25d76045d32ea3f3d5a7fcd83a298
esprit-libre <contact@esprit-libre-conseil.com>
2017-09-13 15:37:22 +0200

    Automation of nextcloud space usage report

--- space_usage.sh
+++ space_usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly VERSION='1.1'
+readonly VERSION='1.2'
 readonly DATE='13 sept. 2017'
 
 CURRENT_DATE=$(date +%d/%m/%Y_%Hh%M)
@@ -12,7 +12,7 @@ version() {
 
 usage() {
 	echo ''
-	echo '$(basename "${0}") -p[ath] <PATH> [ OPTIONS ]'
+	echo '$(basename "${0}") -p[ath] <THEPATH> [ OPTIONS ]'
 	echo ''
 	echo 'OPTIONS = { -v[ersion] | -h[elp] | -d[ebug] }'
 	echo 'OPTIONS = { -l[evel] } : depth to explore, default is 1. }'
@@ -62,7 +62,7 @@ parameters() {
 				readonly NEXTCLOUD='true'
 				;;
 			-p|-path)
-				readonly PATH="${2}"
+				readonly THEPATH="${2}"
 				shift
 				;;
 			-s|-short)
@@ -79,7 +79,7 @@ parameters() {
 		shift
 	done
 
-	if [ ! -d "${PATH}" ]; then
+	if [ ! -d "${THEPATH}" ]; then
 		log "${ERR}Missing or wrong path info."
 		usage
 		return 1
@@ -91,7 +91,7 @@ parameters() {
 			LEVEL=1
 		fi
 	fi
-	if [ -z "${MAILS}" ] || [[ ! "${MAILS}" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; then
+	if [ -n "${MAILS}" ] && [[ ! "${MAILS}" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; then
 		log "${WARN}Invalid mail: ${MAILS}. No mail report."
 		unset MAILS
 	fi
@@ -99,33 +99,61 @@ parameters() {
 
 init
 log "\n---------------------"
-log "${INFO}Starting space usage - ${CURRENT_DATE}"
+log -e "${INFO}Starting space usage - ${CURRENT_DATE}\n\n"
 parameters "$@"
 
+#~ GLOBAL="/usr/bin/du -sh"
 if [ -n "${NEXTCLOUD}" ]; then
 	if [ -n "${SHORT}" ]; then
-		RESULT=$(/usr/bin/du -d 1 -h --exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
+		#~ STATS='/usr/bin/du -d 1 -h'
+		#~ EXCLUDE='--exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
+			#~ --exclude="*files_external*" --exclude="*files_versions*" --exclude="*thumbnails*" \
+			#~ --exclude="*updater_backup*" --exclude="*uploads*" --exclude="*locks*"'
+		#~ STATS=$(${STATS} "${EXCLUDE}" ${THEPATH} | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		STATS=$(/usr/bin/du -d 1 -h --exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
 			--exclude="*files_external*" --exclude="*files_versions*" --exclude="*thumbnails*" \
-			--exclude="*updater_backup*" --exclude="*uploads*" "${PATH}" | \
-			/bin/sed "s/${PATH//\//\\\/}//g")
-		GLOBAL=$(/usr/bin/du -sh --exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
+			--exclude="*updater_backup*" --exclude="*uploads*" --exclude="*locks*" "${THEPATH}" | \
+			/bin/sed "s/${THEPATH//\//\\\/}//g")
+		#~ QUOTA=$(${GLOBAL} "${EXCLUDE}" "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		QUOTA=$(/usr/bin/du -sh --exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
 			--exclude="*files_external*" --exclude="*files_versions*" --exclude="*thumbnails*" \
-			--exclude="*updater_backup*" --exclude="*uploads*" "${PATH}" | \
-			/bin/sed "s/${PATH//\//\\\/}//g")
+			--exclude="*updater_backup*" --exclude="*uploads*" --exclude="*locks*" "${THEPATH}" | \
+			/bin/sed "s/${THEPATH//\//\\\/}//g")
+		#~ GLOBAL=$(${GLOBAL} "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		GLOBAL=$(/usr/bin/du -sh "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
 	else
-		RESULT=$(/usr/bin/du -d 2 -h "${PATH}" | /bin/sed '/files_trashbin/d' | /bin/sed '/cache/d' | \
+		#~ STATS="/usr/bin/du -d 2 -h"
+		#~ EXCLUDE="| /bin/sed '/files_trashbin/d' | /bin/sed '/cache/d' | /bin/sed '/appdata_/d' | \
+			#~ /bin/sed '/files_external/d' | /bin/sed '/files_versions/d' | /bin/sed '/thumbnails/d' | \
+			#~ /bin/sed '/updater_backup/d' | /bin/sed '/uploads/d' | /bin/sed '/locks/d' | \
+			#~ "
+		#~ STATS=$(${STATS} "${THEPATH}" "${EXCLUDE}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		STATS=$(/usr/bin/du -d 2 -h "${THEPATH}" | /bin/sed '/files_trashbin/d' | /bin/sed '/cache/d' | \
 			/bin/sed '/appdata_/d' | /bin/sed '/files_external/d' | /bin/sed '/files_versions/d' | \
 			/bin/sed '/thumbnails/d' | /bin/sed '/updater_backup/d' | /bin/sed '/uploads/d' | \
-			/bin/sed "s/${PATH//\//\\\/}//g")
-		GLOBAL=$(/usr/bin/du -sh "${PATH}" | /bin/sed "s/${PATH//\//\\\/}//g")
+			/bin/sed '/locks/d' | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		QUOTA=$(/usr/bin/du -sh --exclude="*files_trashbin*" --exclude="*cache*" --exclude="*appdata*" \
+			--exclude="*files_external*" --exclude="*files_versions*" --exclude="*thumbnails*" \
+			--exclude="*updater_backup*" --exclude="*uploads*" --exclude="*locks*" "${THEPATH}" | \
+			/bin/sed "s/${THEPATH//\//\\\/}//g")
+		#~ GLOBAL=$(${GLOBAL} "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+		GLOBAL=$(/usr/bin/du -sh "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
 	fi
 else
-	RESULT=$(/usr/bin/du -d "${LEVEL}" -h "${PATH}" | /bin/sed "s/${PATH//\//\\\/}//g")
-	GLOBAL=$(/usr/bin/du -sh "${PATH}" | /bin/sed "s/${PATH//\//\\\/}//g")
+	#~ STATS="/usr/bin/du -d ${LEVEL} -h"
+	#~ STATS=$(${STATS} "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+	STATS=$(/usr/bin/du -d ${LEVEL} -h "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+	#~ GLOBAL=$(${GLOBAL} "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
+	GLOBAL=$(/usr/bin/du -sh "${THEPATH}" | /bin/sed "s/${THEPATH//\//\\\/}//g")
 fi
 
-echo -e "${RESULT}\n------\n"
+
+
+echo -e "${STATS}\n------------\nGLOBAL=${GLOBAL} ( vs QUOTA=${QUOTA})"
+
 if [ -n "${MAILS}" ]; then
-	echo -e "Espace occupé pour ${PATH} - ${CURRENT_DATE}\n\nTotal = ${GLOBAL}\n\n${RESULT}" | \
-		/usr/bin/mail -a "Content-Type: text/plain; charset=UTF-8" -s "[Cloud] Info quota pour ${PATH} - ${CURRENT_DATE}" "${MAILS}"
+echo -e "Espace occupé pour ${THEPATH} - ${CURRENT_DATE}\n\n \
+Occupation...\n...décomptée du quota = ${QUOTA}\n...réelle (sur disque) = ${GLOBAL}\n\n${STATS}" | \
+/usr/bin/mail -a "Content-Type: text/plain; charset=UTF-8" \
+	-s "[Cloud] Info quota pour ${THEPATH} - ${CURRENT_DATE}" "${MAILS}"
 fi
